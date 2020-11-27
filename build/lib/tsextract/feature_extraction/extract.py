8ac4c616137fa7befd3df066ba5b6b963abc487e
cydal <cydalsij@outlook.com>
2020-11-27 09:09:05 +0100

    update setup.py

--- build/lib/tsextract/feature_extraction/extract.py
+++ build/lib/tsextract/feature_extraction/extract.py
@@ -5,26 +5,30 @@ import numpy as np
 import scipy.stats
 
 from datetime import datetime
+import copy
 
 
 
-## Target Lag - T+ for predictive variable
+def build_features_forecast(data, features_request, include_tzero=True):
+    """Performs main feature engineering and returns
+       dataframe without target for forecasting
 
-## todo - write error checks if values differ from what is expected
-#  arguments
-#  window - integer // window_statistics - list, 2 values (int & string)
-#  difference / momentum / force - list, 2 values (int & int)
+    Parameters
+    ----------
 
-## Returns 1-dim vector of summary stats
-## Expects 2 dim matrix
-def win_stat(data, fun_c):
-  return(np.expand_dims(fun_c(data), axis=1))
+    data : Series or vector
+        Time series data to perform operation on
+    features_request : Dictionary
+        Dict containing feature engineering requests
+    include_tzero : bool, optional
+        Whether or not to include column t+0
 
-## when building features, minimum required argument is windows size
-## Automate feature name passed in to feature variable
-# will allow to handle errors all at once
-
-## implement disjointed backwards lag e.g t-24 to t-48 // t-48 to t-96 e.t.c
+    Returns
+    -------
+    DataFrame
+        Pandas df containing features only
+    """
+    return(_build(data, features_request, target_lag=-1, include_tzero=include_tzero, forecast=True))
 
 
 def build_features(data, features_request, target_lag=3, include_tzero=True):
@@ -47,6 +51,34 @@ def build_features(data, features_request, target_lag=3, include_tzero=True):
     DataFrame
         Pandas df containing features and target
     """
+    return(_build(data, features_request, target_lag=target_lag, include_tzero=include_tzero, forecast=False))
+
+
+
+## Target Lag - T+ for predictive variable
+
+## todo - write error checks if values differ from what is expected
+#  arguments
+#  window - integer // window_statistics - list, 2 values (int & string)
+#  difference / momentum / force - list, 2 values (int & int)
+
+## Returns 1-dim vector of summary stats
+## Expects 2 dim matrix
+def _win_stat(data, fun_c):
+  return(np.expand_dims(fun_c(data), axis=1))
+
+## when building features, minimum required argument is windows size
+## Automate feature name passed in to feature variable
+# will allow to handle errors all at once
+
+## implement disjointed backwards lag e.g t-24 to t-48 // t-48 to t-96 e.t.c
+
+
+## Build features helper function
+def _build(data, features_request, target_lag=3, include_tzero=True, forecast=False):
+
+    features_request = copy.deepcopy(features_request)
+
     ## window format
     if "window" not in features_request.keys():
         raise Exception("Features request must contain at the least, a window OP")
@@ -118,30 +150,30 @@ def build_features(data, features_request, target_lag=3, include_tzero=True):
 
     # Helper functions to run different operations
     def window(features_item):
-        return(window_list(data, features_item))
+        return(_window_list(data, features_item))
 
     def window_statistic(features_item):
-        return(win_stat(window_list(data, features_item[:-1]), features_item[-1]))
+        return(_win_stat(_window_list(data, features_item[:-1]), features_item[-1]))
 
     ## Confirm - :-1 --- -1
     def difference(features_item): ## 10 24 10
-        return(difference_comb(data, features_item))
+        return(_difference_comb(data, features_item))
 
     def momentum(features_item):
-        return(momentum_comb(data, features_item))
+        return(_momentum_comb(data, features_item))
 
     def force(features_item):
-        return(force_comb(data, features_item))
+        return(_force_comb(data, features_item))
 
     ### Confirm  1-2 windowing
     def difference_statistic(features_item):
-        return(win_stat(difference_comb(data, features_item[:-1]), features_item[-1]))
+        return(_win_stat(_difference_comb(data, features_item[:-1]), features_item[-1]))
 
     def momentum_statistic(features_item):
-        return(win_stat(momentum_comb(data, features_item[:-1]), features_item[-1]))
+        return(_win_stat(_momentum_comb(data, features_item[:-1]), features_item[-1]))
 
     def force_statistic(features_item):
-        return(win_stat(force_comb(data, features_item[:-1]), features_item[-1]))
+        return(_win_stat(_force_comb(data, features_item[:-1]), features_item[-1]))
 
     ## Store feature names of type statistic
     stat_features = ["window_statistic", "difference_statistic",
@@ -161,8 +193,10 @@ def build_features(data, features_request, target_lag=3, include_tzero=True):
         tzero = pd.DataFrame(np.expand_dims(data, axis=1), columns=["tzero"])
 
     # Get T+ target
-    target = pd.DataFrame(np.expand_dims(data.shift(-target_lag), axis=1),
-                            columns=["Target_Tplus{0}".format(str(target_lag))])
+    ## -1 means it's a forecast request
+    if target_lag != -1:
+        target = pd.DataFrame(np.expand_dims(data.shift(-target_lag), axis=1),
+                                columns=["Target_Tplus{0}".format(str(target_lag))])
 
     ### Loop through features
     ## Ensure window data is processed first
@@ -213,20 +247,20 @@ def build_features(data, features_request, target_lag=3, include_tzero=True):
         df = pd.concat([df, tzero], axis=1)
 
     ## Add target column
-    df = pd.concat([df, target], axis=1)
+    #### Add forecast conditional
+    ##### Check if it's a forecast
+    if target_lag != -1:
+        df = pd.concat([df, target], axis=1)
 
     # Set index of original dataframe
     df["Date"] = data.index
     df = df.set_index("Date")
 
     ## Cut off NaNs
-    df = cut_final(df)
+    df = _cut_final(df, forecast=forecast)
 
     # Remove NaN values
     return(df)
-
-
-
 
 
 ## Description takes in 1-dim data, T+, window size
@@ -234,7 +268,7 @@ def build_features(data, features_request, target_lag=3, include_tzero=True):
 ### f_idx - T+ value /// leave @ 0 for the moment. may be irrelevant
 ### window - window - size
 
-def window_list(data, window):
+def _window_list(data, window):
   if len(window) == 1:
     return(np.vstack([data.shift(i + 1) for i in range(window[0])][::-1]).T)
   else:
@@ -251,19 +285,19 @@ def window_list(data, window):
 
 
 ## Difference/Momentum/Force helper functions
-def difference(data, lag):
+def _difference(data, lag):
   return(data.diff(lag))
 
-def momentum(data, lag):
-  return(difference(difference(data, lag), lag))
+def _momentum(data, lag):
+  return(_difference(_difference(data, lag), lag))
 
-def force(data, lag):
-  return(difference(momentum(data, lag), lag))
+def _force(data, lag):
+  return(_difference(_momentum(data, lag), lag))
 
 
 
 ## Perform diff/mom/force operations
-def difference_comb(data, window_lag):
+def _difference_comb(data, window_lag):
   ## Check size of window_size
   if len(window_lag) == 2:
     ## Number of features to return
@@ -277,15 +311,15 @@ def difference_comb(data, window_lag):
 
   if len(window_lag) == 2:
     ## Difference Matrix
-    multi_diff = difference(pd.DataFrame(window_list(data, [window_lag[0]-1])), window_lag[1])
-    single_diff = difference(data, window_lag[1])
+    multi_diff = _difference(pd.DataFrame(_window_list(data, [window_lag[0]-1])), window_lag[1])
+    single_diff = _difference(data, window_lag[1])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[1]:]
   else:
     ## Difference Matrix
-    multi_diff = difference(pd.DataFrame(window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
-    single_diff = difference(data, window_lag[2])
+    multi_diff = _difference(pd.DataFrame(_window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
+    single_diff = _difference(data, window_lag[2])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[2]:]
@@ -297,7 +331,7 @@ def difference_comb(data, window_lag):
 
   #return(pd.DataFrame(diffed))
 
-def momentum_comb(data, window_lag):
+def _momentum_comb(data, window_lag):
 
   ## Check size of window_size
   if len(window_lag) == 2:
@@ -312,15 +346,15 @@ def momentum_comb(data, window_lag):
 
   if len(window_lag) == 2:
     ## Momentum Matrix
-    multi_diff = momentum(pd.DataFrame(window_list(data, [window_lag[0]-1])), window_lag[1])
-    single_diff = momentum(data, window_lag[1])
+    multi_diff = _momentum(pd.DataFrame(_window_list(data, [window_lag[0]-1])), window_lag[1])
+    single_diff = _momentum(data, window_lag[1])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[1]:]
   else:
     ## Momentum Matrix
-    multi_diff = momentum(pd.DataFrame(window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
-    single_diff = momentum(data, window_lag[2])
+    multi_diff = _momentum(pd.DataFrame(_window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
+    single_diff = _momentum(data, window_lag[2])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[2]:]
@@ -330,7 +364,7 @@ def momentum_comb(data, window_lag):
 
   return(diffed)
 
-def force_comb(data, window_lag):
+def _force_comb(data, window_lag):
 
   ## Check size of window_size
   if len(window_lag) == 2:
@@ -344,15 +378,15 @@ def force_comb(data, window_lag):
 
   if len(window_lag) == 2:
     ## Force Matrix
-    multi_diff = difference(pd.DataFrame(window_list(data, [window_lag[0]-1])), window_lag[1])
-    single_diff = difference(data, window_lag[1])
+    multi_diff = _difference(pd.DataFrame(_window_list(data, [window_lag[0]-1])), window_lag[1])
+    single_diff = _difference(data, window_lag[1])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[1]:]
   else:
     ## Force Matrix
-    multi_diff = difference(pd.DataFrame(window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
-    single_diff = difference(data, window_lag[2])
+    multi_diff = _difference(pd.DataFrame(_window_list(data,[window_lag[0]-1 , window_lag[1]-1])), window_lag[2])
+    single_diff = _difference(data, window_lag[2])
 
     multi_size = multi_diff.shape[1]
     multi_diff = multi_diff.iloc[:, window_lag[2]:]
@@ -365,14 +399,14 @@ def force_comb(data, window_lag):
 ### Get number of NaN values to drop
 ### returns NaNs at start & bottom of last T+ column.
 
-def get_num_nan(df):
+def _get_num_nan(df):
   num_nan = df.isnull().sum()
   return([max(num_nan[0:-1]), list(num_nan)[-1]])
 
 ## 2-d Matrix to Pandas Dataframe
 ## adds global index as DF index
 ## adds convenience for combination later
-def build_data(data, index):
+def _build_data(data, index):
   data_df = pd.DataFrame(data)
   data_df["Date"] = index
   data_df = data_df.set_index("Date")
@@ -380,7 +414,12 @@ def build_data(data, index):
 
 ## Re-Centers data after multiple shifts
 ## uses num_nan output
-def cut_final(df):
-  h, t = get_num_nan(df)
+def _cut_final(df,forecast=False):
+  
+  h, t = _get_num_nan(df)
 
-  return(df[h:][:-t])
+  if forecast == False:
+    return(df[h:][:-t])
+  else:
+    return(df[h:])
+    
